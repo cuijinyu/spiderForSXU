af19d69f4080498bb42b4c9aa9a8f36aedffcbce
崔晋瑜 <2510271615@qq.com>
2017-07-17 16:49:24 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,33 @@
  * Created by 崔晋瑜 on 2017/7/14.
  */
 var express = require('express');
+var form=require('formidable');
+var spider=require("../spider/spider")
 var router = express.Router();
-
+var message={
+    id:'',
+    password:'',
+    checkNumber:''
+};
+var sxuSpider=new spider();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express',describe:'OK' });
+    sxuSpider.getCheckNumber();
+    setTimeout(function () {
+        res.render('index', { title: 'Express',describe:'OK' });
+    },3000);
 });
 router.post('/getscore',function(req,res,next){
     console.log(req.body);
+    message.id=req.body.id;
+    message.password=req.body.password;
+    message.checkNumber=req.body.checkNumber;
+    sxuSpider.login(message.id,message.password,message.checkNumber,function () {
+        res.render('score','');
+    })
+});
+router.get('/score',function (req,res,next) {
+    res.render('score','');
 });
 module.exports = router;
+module.exports.message=message;
