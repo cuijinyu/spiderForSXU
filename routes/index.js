4e383d3891e506e22797a22070cfa753390c5ba0
崔晋瑜 <2510271615@qq.com>
2017-07-18 10:11:57 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -10,22 +10,44 @@ var message={
     password:'',
     checkNumber:''
 };
-var sxuSpider=new spider();
+var count=0;
+var spiders=[];
+var spiderSXU1=new spider();
+var spiderSXU2=new spider();
+var spiderSXU3=new spider();
+spiders.push(spiderSXU1);
+spiders.push(spiderSXU2);
+spiders.push(spiderSXU3);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    sxuSpider.getCheckNumber();
+    if(count==2){
+        count=0;
+    }else{
+        count++;
+    }
+    spiders[count].setID(count);
+    spiders[count].getCheckNumber();
+    res.cookie("id",count);
     setTimeout(function () {
         res.render('index', { title: 'Express',describe:'OK' });
-    },3000);
+    },10000);
 });
 router.post('/getscore',function(req,res,next){
+    var theSpider;
+    console.log(req.cookies);
+    for(i=0;i<spiders.length;i++){
+        if(req.cookies.id==spiders[i].getID())
+        {
+           theSpider=spiders[i];
+        }
+    }
     console.log(req.body);
     message.id=req.body.id;
     message.password=req.body.password;
     message.checkNumber=req.body.checkNumber;
-    sxuSpider.login(message.id,message.password,message.checkNumber,function () {
-        res.render('score','');
-    })
+    theSpider.login(message.id,message.password,message.checkNumber,function () {
+        res.render('score','');}
+    )
 });
 router.get('/score',function (req,res,next) {
     res.render('score','');
